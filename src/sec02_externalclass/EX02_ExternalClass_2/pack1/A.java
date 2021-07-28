b05fad3758ea800d6de8035936c141f4eefbf83b
jeffrey2465 <jeffrey2465@gmail.com>
2021-07-28 14:39:01 +0900

    code modification

--- src/sec02_externalclass/EX02_ExternalClass_2/pack1/A.java
+++ src/sec02_externalclass/EX02_ExternalClass_2/pack1/A.java
@@ -1,4 +1,4 @@
-package sec02_externalclass.EX02_ExternalClass_2;
+package sec02_externalclass.EX02_ExternalClass_2.pack1;
 
 /*외부클래스를 별도의 파일로 분리*/
 
@@ -6,7 +6,7 @@ public class A {
 	int m=3;
 	int n=4;
 	
-	void print() {
+	public void print() {
 		System.out.println(m+", "+n);
 	}
 }
--- src/sec02_externalclass/EX02_ExternalClass_2/pack2/ExternalClass_2.java
+++ src/sec02_externalclass/EX02_ExternalClass_2/pack2/ExternalClass_2.java
@@ -1,10 +1,12 @@
-package sec02_externalclass.EX02_ExternalClass_2;
+package sec02_externalclass.EX02_ExternalClass_2.pack2;
+
+import sec02_externalclass.EX02_ExternalClass_2.pack1.A;
 
 /*외부클래스를 별도의 파일로 분리*/
 
 public class ExternalClass_2 {
 	public static void main(String[] args) {
 		A a = new A();
-		a.print(); //3, 4
+		a.print(); //3, 4 
 	}
 }
